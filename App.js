aebb5271623864c33e12204bae7308153a03d6af
tinghaoma <tinghaoma@sohu-inc.com>
2018-10-10 23:36:30 +0800

    使用Navigator

--- App.js
+++ App.js
@@ -7,43 +7,63 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Text, View, Image} from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedTab: 'home'
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TabNavigator>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'home'}
+                        selectedTitleStyle={{color:'red'}}
+                        title="Home"
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_polular.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')}/>}
+                        badgeText="2"
+                        onPress={() => this.setState({selectedTab: 'home'})}>
+                        <View style={styles.page1}/>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'profile'}
+                        selectedTitleStyle={{color:'blue'}}
+                        title="Profile"
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'blue'}]} source={require('./res/images/ic_trending.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'profile'})}>
+                        <View style={styles.page2}/>
+                    </TabNavigator.Item>
+                </TabNavigator>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    page1: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    page2: {
+        flex: 1,
+        backgroundColor: 'green'
+    },
+    image:{
+        width:22,
+        height:22
+    }
 });
