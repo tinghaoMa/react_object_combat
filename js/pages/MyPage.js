c6d7cfb4fbe81649bba0cc43fcb9c4d301c2be8d
tinghaoma <tinghaoma@sohu-inc.com>
2018-10-17 15:47:29 +0800

    alert 使用

--- js/pages/MyPage.js
+++ js/pages/MyPage.js
@@ -9,7 +9,8 @@ import {
     Text,
     View,
     Image,
-    ScrollView
+    ScrollView,
+    Alert
 } from 'react-native';
 import CheckBox from 'react-native-check-box';
 import NavigationBar from '../common/NavigationBar';
@@ -56,7 +57,16 @@ export default class MyPage extends Component {
     }
 
     leftBtnClick() {
-
+        Alert.alert(
+            'Alert Title',
+            'My Alert Msg',
+            [
+                {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+                {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            { cancelable: true }
+        )
     }
 
     rightBtnClick() {
